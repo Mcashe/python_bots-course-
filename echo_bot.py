d2cca0c4e7889141e94325130dbbbc80a205c773
Dmitrii Bakaev <bakaevdima4@gmail.com>
2025-07-26 15:23:38 +0300

    updated echo_bot

--- echo_bot.py
+++ echo_bot.py
@@ -1,6 +1,8 @@
 from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
 from aiogram.types import Message
+from aiogram.types import ContentType
+from aiogram import F
 
 
 BOT_TOKEN = '7938316152:AAE_J10eHZHnjdHXOOokda6GLguIjOTmfHM'
@@ -20,9 +22,21 @@ async def process_help_command(message: Message):
         'я пришлю тебе твое сообщение'
     )
 
-@dp.message()
-async def send_echo(message: Message):
-    await bot.send_message(message.chat.id, message.text)
+@dp.message(F.photo)
+async def send_photo_echo(message: Message):
+    await message.answer_photo(message.photo[0].file_id)
+
+@dp.message(F.audio)
+async def send_audio_echo(message: Message):
+    await message.answer_audio(message.auido.file_id)
+
+@dp.message(F.sticker)
+async def send_audio_echo(message: Message):
+    await message.answer_audio(message.sticker.file_id)
+
+@dp.message(F.audio_note)
+async def send_audio_echo(message: Message):
+    await message.answer_audio_note(message.auido.file_id)
 
 if __name__ == '__main__':
     dp.run_polling(bot)
